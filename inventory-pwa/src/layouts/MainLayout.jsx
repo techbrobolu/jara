f5067819d8dbcaa3348d970d656a3d4ec82f4e75
techbrobolu <techbrobolu@gmail.com>
2025-10-15 14:33:58 +0100

    fix imports #2

--- inventory-pwa/src/layouts/MainLayout.jsx
+++ inventory-pwa/src/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/common/Header';
-import { useTheme } from '../hooks/useTheme';
+import useTheme from '../hooks/useTheme';
 import { Outlet } from 'react-router';
 
 const MainLayout = () => {
